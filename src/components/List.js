c1923f4f5031b9eead2cefc95c4369975eed7a71
Kübra Özdemir <kbraozdemir18@gmail.com>
2024-08-14 21:28:44 +0300

    toggle işlemleri yapıldı.

--- src/components/List.js
+++ src/components/List.js
@@ -1,31 +1,24 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggle } from '../redux/todos/todosSlice';
 
 export default function List() {
+    const dispatch = useDispatch();
+    
     const items = useSelector (state => state.todos.items);
-    
-    
 
     return (
     <ul className='todo-list'>
-            {/* <li className='completed'>
+        {
+            items.map((item) => (
+            <li key={item.id} className={item.completed ? 'completed' : ''}>
                 <div className='view'>
-                    <input className='toggle' type='checkbox' />
-                    <label htmlFor='toggle-all'>Learn JavaScript</label>
+                    <input className='toggle' type='checkbox' onChange={() => dispatch(toggle({ id: item.id}))} />
+                    <label htmlFor='toggle-all'>{item.title}</label>
                     <button className='destroy'></button>
                 </div>
-            </li> */}
-            
-            {
-                items.map((item) => (
-                    <li key={item.id} className={item.completed ? 'completed' : ''}>
-                        <div className='view'>
-                            <input className='toggle' type='checkbox' />
-                            <label htmlFor='toggle-all'>{item.title}</label>
-                            <button className='destroy'></button>
-                        </div>
-                    </li>
-                ))}
-        </ul>
+            </li>
+        ))}
+    </ul>
   )
 }
