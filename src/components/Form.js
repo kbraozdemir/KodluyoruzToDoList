6f82b72db1e2e36d37eb5b8f974a983a8dbc9ad3
Kübra Özdemir <kbraozdemir18@gmail.com>
2024-09-01 01:36:07 +0300

    API üzerinden eklenme düzenlendi (1 hata var)

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,42 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { addTodo } from '../redux/todos/todosSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodoAsync } from '../redux/todos/todosSlice';
+import Loading from './Loading';
+import Error from './Error';
 
 export default function Form() {
   const [title, setTitle] = useState('');
   
-
   const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
+  const isLoading = useSelector (state => state.todos.addNewTodoLoading)
+
+  const error = useSelector (state => state.todos.addNewTodoError)
+
+  const handleSubmit = async (e) => {
+    if (!title) return;
+
     e.preventDefault();
 
-    dispatch(addTodo ({title}));
+    await dispatch(addTodoAsync ({title}));
     setTitle('');
   };
 
   return (
     <div>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} style={{display: "flex", alignItems: "center"}}>
             <input 
+            disabled= {isLoading}
             className='new-todo'
             placeholder='what needs to be done?' 
             autoFocus 
             value={title} 
             onChange={(e) => setTitle(e.target.value)} />
+            {
+              isLoading && <Loading />
+            }
+
+            {error && <Error message={error} />}
         </form>
     </div>
   )
--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -1,8 +1,14 @@
-import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export const getTodosAsync = createAsyncThunk('todos/getTodosAsync', async () => {
-    const res = await fetch('http://localhost:7000/todos');
-    return await res.json();
+    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/todos`);
+    return res.data;
+});
+
+export const addTodoAsync = createAsyncThunk('todos/addTodoAsync', async (data) => {
+    const res = await axios.post(`${process.env.REACT_APP_API_BASE_ENDPOINT}/todos` , data);
+    return res.data;
 });
 
 const todosSlice = createSlice ({
@@ -11,24 +17,11 @@ const todosSlice = createSlice ({
         items: [],
         isLoading: false,
         error: null,
-        
-    activeFilter: 'all',
+        activeFilter: 'all',
+        addNewTodoLoading: false,
+        addNewTodoError: null,
     },
     reducers: {  // Reducer, state'in nasıl güncelleneceğini tanımlar. Action type'larına göre yeni state'i oluşturur.
-        addTodo: {
-            reducer:(state, action) => {
-                state.items.push(action.payload);
-            },
-            prepare: ({title}) => {
-                return {
-                    payload: {
-                        id: nanoid(),
-                        completed: false,
-                        title,
-                    },
-                };
-            },
-        },
         toggle: (state, action) => {
             const {id} = action.payload; //Action, bir olayın veya işlemin temsilcisidir. Bir şeyi eklemek, silmek veya güncellemek istediğinizde bir action oluşturursunuz.
 
@@ -52,6 +45,7 @@ const todosSlice = createSlice ({
     },
     extraReducers: (builder) => {  //builder kullanarak daha esnek bir şekilde yönetmek, asenkron işlemlerinin daha temiz bir şekilde ele alınmasını sağlar.
         builder
+            // get Todo
             .addCase(getTodosAsync.pending, (state, action) => {
                 state.isLoading = true;
             })
@@ -62,7 +56,22 @@ const todosSlice = createSlice ({
             .addCase(getTodosAsync.rejected, (state, action) => {
                 state.isLoading = false;
                 state.error = action.error.message;
-            });
+            })
+
+            //add Todo
+            .addCase(addTodoAsync.fulfilled, (state, action) => {
+                state.items.push(action.payload);
+                state.addNewTodoLoading = false;
+            })
+
+            .addCase(addTodoAsync.pending, (state, action) => {
+                state.addNewTodoLoading = true;
+            })
+
+            .addCase(addTodoAsync.rejected, (state, action) => {
+                state.addNewTodoLoading = false;
+                state.addNewTodoError = action.error.message;
+            })
     }
 });
 
@@ -78,5 +87,5 @@ export const selectFilteredTodos = (state) => {
 export const selectTodos = (state) => state.todos.items;
 export const selectActiveFilter = (state) => state.todos.activeFilter;
 
-export const { addTodo, toggle, destroy,changeActiveFilter, clearCompleted } = todosSlice.actions;
+export const { toggle, destroy,changeActiveFilter, clearCompleted } = todosSlice.actions;
 export default todosSlice.reducer;
